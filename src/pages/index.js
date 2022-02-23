27ba4391493bf8be7f7d7bf499f5ebe9aa34f2be
Josiah Luikham <cimmerian62@gmail.com>
2022-02-23 14:14:31 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Lord Krishna, playing a flute, with a deer on his right and a peacock to his left."
+        src="../images/krishna.png"
+      />
     </Layout>
   )
 }
